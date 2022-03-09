bcd3ecebff52cd84852f2b8f4db1fab53b9935da
Hai Nguyen <hhai2105@gmail.com>
2022-03-09 09:02:28 -0500

    added reducers to work with redux

---
+++ frontend/src/reducers/User.js
@@ -0,0 +1,17 @@
+function userReducer(state = {userData : null, hasFailed : true}, action){
+	switch(action.type){
+	case "LOGIN":
+		localStorage.setItem('profile', JSON.stringify({...action.payload.data}));
+		return { ...state, authData: action.data, hasFailed: false};
+	case "LOGOUT":
+		localStorage.clear()
+		return { ...state, authData: null, hasFailed: false};
+	case "FAILED":
+		return { ...state, hasFailed: true};
+	default:
+		return state;
+	}
+
+}
+
+export default userReducer

ae97eab3e82a0c9022d2e79b43494b5e8503a820
Samuel-Jay <37205009+Samuel-Jay@users.noreply.github.com>
2022-04-04 15:30:21 -0400

    Update TrainingAssn.js

--- backend/models/TrainingAssn.js
+++ backend/models/TrainingAssn.js
@@ -4,11 +4,15 @@ const Schema = mongoose.Schema;
 
 const TrainSchema = new Schema(
   {
-    requesterId: { type: Number, required: false },
-    traineeId: { type: Number, required: false },
-    source: { type: String, required: false },
-    dueDate: { type: Date, required: false },
-    status: { type: Boolean, required: false },
+    taskId: {type: Number, required: true},
+    assignerEmail: { type: String, required: true },
+    assigneeEmail: { type: String, required: true },
+    taskName: {type: String, required: true},
+    taskLink: {type: String, required: true},
+    taskDescription: {type: String, required: true},
+    startDate: {type: Date, required: true},
+    dueDate: { type: Date, required: true },
+    status: { type: Boolean, required: true },
   },
   {
     timestamps: true,

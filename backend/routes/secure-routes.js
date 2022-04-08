7ec587f7fa4c2bba870b749808d61c1f84a857f1
Sahil Gupta <sahgupta@umass.edu>
2022-04-07 20:06:17 -0400

    updated Training task api with using employee and manager Ids instead of emails

--- backend/routes/secure-routes.js
+++ backend/routes/secure-routes.js
@@ -7,6 +7,11 @@ const userSchema = require("../schema/UserSchema");
 const { companies } = require("../config");
 
 const generateHash = (key) => {
+  // var today = new Date();
+  // var date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
+  // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  // var dateTime = date + ' ' + time;
+  // var key = dateTime
   var hash = 0;
   if (key.length == 0) return hash;
   for (i = 0; i < key.length; i++) {
@@ -25,62 +30,109 @@ router.get("/profile", (req, res, next) => {
   });
 });
 
-router.post("/trainingTask/create", (req, res, next) => {
-  try {
-    const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
-    const User = mongoose.connection
-      .useDb(companyDB)
-      .model("users", userSchema);
-    const TrainingTask = mongoose.connection
-      .useDb(companyDB)
-      .model("assignTraining", trainingSchema);
+router.post("/trainingTask/create", async (req, res, next) => {
+  const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
+  const User = mongoose.connection.useDb(companyDB).model("users", userSchema);
+  const TrainingTask = mongoose.connection
+    .useDb(companyDB)
+    .model("assignTraining", trainingSchema);
 
-    User.findOne({ email: req.body.assignerEmail }, async (err, admin) => {
-      if (!admin) {
-        return res.json({ code: 404, message: "Assigner email doesn't exist" });
-      }
-      if (admin.positionTitle !== "CEO") {
-        return res.json({
-          code: 401,
-          message: "Assigner does not have Admin permissions",
-        });
-      }
-      User.find({}, async (err, users) => {
-        emails = users
-          .filter((user) => user.positionTitle !== "CEO")
-          .map((user) => user.email);
-        emails.forEach(async (email) => {
-          var taskData = {
-            taskId: Math.abs(
-              generateHash(
-                req.body.assignerEmail +
-                  req.body.taskName +
-                  req.body.taskLink +
-                  req.body.startDate +
-                  req.body.dueDate
-              )
-            ),
-            assignerEmail: req.body.assignerEmail,
-            assigneeEmail: email,
-            taskName: req.body.taskName,
-            taskLink: req.body.taskLink,
-            taskDescription: req.body.taskDescription,
-            startDate: req.body.startDate,
-            dueDate: req.body.dueDate,
-            status: "Incomplete",
-          };
-          const trainingTask = await TrainingTask.create(taskData);
-          await trainingTask.save();
-        });
-        return res.json({
-          code: 200,
-          message: "Successfully added training tasks",
-        });
+  await User.findOne({ employeeId: req.body.assignerId }).then(async (admin) => {
+    if (!admin) {
+      return res.json({ code: 404, message: "Assigner email doesn't exist" });
+    }
+    if (admin.positionTitle !== "CEO") {
+      return res.json({
+        code: 401,
+        message: "Assigner does not have Admin permissions",
       });
-    });
-  } catch (error) {
-    return res.json(error);
-  }
+    }
+    await User.find({}).then((users) => {
+      employeeIds = users.filter((user) => user.positionTitle !== "CEO").map((user) => user.employeeId);
+      employeeIds.forEach(async (employeeId) => {
+        var taskData = {
+          taskId: Math.abs(
+            generateHash(
+              req.body.assignerId +
+              req.body.taskName +
+              req.body.startDate +
+              req.body.dueDate
+            )
+          ),
+          assignerId: req.body.assignerId,
+          assigneeId: employeeId,
+          taskName: req.body.taskName,
+          taskLink: req.body.taskLink,
+          taskDescription: req.body.taskDescription,
+          startDate: req.body.startDate,
+          dueDate: req.body.dueDate,
+          status: "Incomplete",
+        };
+        const trainingTask = await TrainingTask.create(taskData);
+        await trainingTask.save();
+      });
+      return res.json({
+        code: 200,
+        message: "Successfully added training tasks",
+      });
+    }).catch(error => {})
+  }).catch(error=> {});
+
+  // try {
+  //   const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
+  //   const User = mongoose.connection
+  //     .useDb(companyDB)
+  //     .model("users", userSchema);
+  //   const TrainingTask = mongoose.connection
+  //     .useDb(companyDB)
+  //     .model("assignTraining", trainingSchema);
+
+  //   User.findOne({ email: req.body.assignerEmail }, async (err, admin) => {
+  //     if (!admin) {
+  //       return res.json({ code: 404, message: "Assigner email doesn't exist" });
+  //     }
+  //     if (admin.positionTitle !== "CEO") {
+  //       return res.json({
+  //         code: 401,
+  //         message: "Assigner does not have Admin permissions",
+  //       });
+  //     }
+  //     User.find({}, async (err, users) => {
+  //       emails = users
+  //         .filter((user) => user.positionTitle !== "CEO")
+  //         .map((user) => user.email);
+  //       emails.forEach(async (email) => {
+  //         var taskData = {
+  //           taskId: Math.abs(
+  //             generateHash(
+  //               req.body.assignerEmail +
+  //                 req.body.taskName +
+  //                 req.body.taskLink +
+  //                 req.body.startDate +
+  //                 req.body.dueDate
+  //             )
+  //           ),
+  //           assignerEmail: req.body.assignerEmail,
+  //           assigneeEmail: email,
+  //           taskName: req.body.taskName,
+  //           taskLink: req.body.taskLink,
+  //           taskDescription: req.body.taskDescription,
+  //           startDate: req.body.startDate,
+  //           dueDate: req.body.dueDate,
+  //           status: "Incomplete",
+  //         };
+  //         const trainingTask = await TrainingTask.create(taskData);
+  //         await trainingTask.save();
+  //       });
+  //       return res.json({
+  //         code: 200,
+  //         message: "Successfully added training tasks",
+  //       });
+  //     });
+  //   });
+  // } catch (error) {
+  //   return res.json(error);
+  // }
 });
 
 router.get("/trainingTask/get", async (req, res, next) => {
@@ -89,7 +141,8 @@ router.get("/trainingTask/get", async (req, res, next) => {
     .useDb(companyDB)
     .model("assignTraining", trainingSchema);
   userTrainingTasks = {};
-  await TrainingTask.find({ assigneeEmail: req.body.requestorEmail })
+
+  await TrainingTask.find({ assigneeId: req.body.requestorId })
     .then((tasks) => {
       if (!tasks) {
         return res.json({
@@ -105,7 +158,8 @@ router.get("/trainingTask/get", async (req, res, next) => {
         message: err.message,
       });
     });
-  await TrainingTask.find({ assignerEmail: req.body.requestorEmail })
+
+  await TrainingTask.find({ assigneeId: req.body.requestorId })
     .then((tasks) => {
       if (!tasks) {
         return res.json({
@@ -124,13 +178,13 @@ router.get("/trainingTask/get", async (req, res, next) => {
   return res.json(userTrainingTasks);
 });
 
-router.patch("/trainingTask/edit", (req, res, next) => {
+router.patch("/trainingTask/edit", async (req, res, next) => {
   const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
   const TrainingTask = mongoose.connection
     .useDb(companyDB)
     .model("assignTraining", trainingSchema);
 
-  TrainingTask.findOne({ taskId: req.body.taskId })
+  await TrainingTask.findOne({ taskId: req.body.taskId })
     .then((task) => {
       if (!task) {
         return res.json({
@@ -138,11 +192,11 @@ router.patch("/trainingTask/edit", (req, res, next) => {
           message: "No task with such credentials found",
         });
       }
-      if (req.body.requestorEmail === task.assignerEmail) {
+      if (req.body.requestorId === task.assignerId) {
         TrainingTask.updateMany(
           {
             taskId: req.body.taskId,
-            assignerEmail: req.body.requestorEmail,
+            assignerId: req.body.requestorId,
           },
           {
             ...req.body,
@@ -161,13 +215,12 @@ router.patch("/trainingTask/edit", (req, res, next) => {
         TrainingTask.updateOne(
           {
             taskId: req.body.taskId,
-            assigneeEmail: req.body.requestorEmail,
+            assigneeId: req.body.requestorId,
           },
           {
             ...req.body,
           }
-        )
-          .then(() => {
+        ).then(() => {
             return res.json({
               code: 200,
               message: "Training Task updated successfully",
@@ -184,173 +237,205 @@ router.patch("/trainingTask/edit", (req, res, next) => {
 });
 
 router.delete("/trainingTask/delete", (req, res, next) => {
-  try {
-    const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
-    const TrainingTask = mongoose.connection
-      .useDb(companyDB)
-      .model("assignTraining", trainingSchema);
+  const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
+  const TrainingTask = mongoose.connection
+    .useDb(companyDB)
+    .model("assignTraining", trainingSchema);
 
-    TrainingTask.findOne(
-      {
-        taskId: req.body.taskId,
-        assignerEmail: req.body.assignerEmail,
-      },
-      (err, task) => {
-        if (!task) {
-          return res.json({
-            code: 404,
-            message: "No task with such credentials found",
-          });
-        }
-        TrainingTask.deleteMany({
-          taskId: req.body.taskId,
-          assignerEmail: req.body.assignerEmail,
-        })
-          .then(() => {
-            return res.json({
-              code: 200,
-              message: "Tasks deleted successfully",
-            });
-          })
-          .catch((err) => {
-            return res.json(err);
-          });
-      }
-    );
-  } catch (error) {
-    return res.json(error);
-  }
-});
-
-router.post("/pto/create", (req, res, next) => {
-  try {
-    const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
-    const User = mongoose.connection
-      .useDb(companyDB)
-      .model("users", userSchema);
-    const PTOTask = mongoose.connection
-      .useDb(companyDB)
-      .model("PTORequests", ptoSchema);
-
-    User.findOne({ email: req.body.requestorEmail }, async (err, employee) => {
-      if (!employee) {
-        return res.json({ code: 404, message: "Employee not found" });
-      }
-      if (employee.positionTitle == "CEO") {
-        return res.json({
-          code: 401,
-          message: "The CEO does not need to request for Paid Time Off",
-        });
-      }
-      User.findOne({ email: req.body.managerEmail }, async (err, manager) => {
-        if (!manager) {
-          return res.json({ code: 404, message: "Manager not found" });
-        }
-        if (employee.managerId != manager.employeeId) {
-          return res.json({
-            code: 401,
-            message: "Invalid Manager credentials",
-          });
-        }
-        var taskData = {
-          taskId: Math.abs(
-            generateHash(employee.email + req.body.startDate + req.body.endDate)
-          ),
-          requestorEmail: req.body.requestorEmail,
-          managerEmail: req.body.managerEmail,
-          managerId: manager.managerId,
-          title: req.body.title,
-          startDate: req.body.startDate,
-          endDate: req.body.endDate,
-          reason: req.body.reason,
-          status: "Pending",
-        };
-
-        const ptoRequest = await PTOTask.create(taskData);
-        await ptoRequest.save();
+  await TrainingTask.findOne({
+    taskId: req.body.taskId,
+    assignerId: req.body.assignerId,
+  }).then(task => {
+    if (!task) {
+      return res.json({
+        code: 404,
+        message: "No task with such credentials found",
+      });
+    }
+    await TrainingTask.deleteMany({
+      taskId: req.body.taskId,
+      assignerId: req.body.assignerId,
+    }).then(() => {
         return res.json({
           code: 200,
-          message: "Successfully added Paid Time Off Request",
+          message: "Tasks deleted successfully",
         });
-      });
-    });
-  } catch (error) {
-    return res.json(error);
-  }
-});
-
-router.patch("pto/edit", (req, res, next) => {
-  try {
-    const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
-    const PTOTask = mongoose.connection
-      .useDb(companyDB)
-      .model("PTORequests", ptoSchema);
-    PTOTask.findOne(
-      {
-        taskId: req.body.taskId,
-        requestorEmail: req.body.requestorEmail,
-      },
-      (err, task) => {
-        if (!task) {
-          return res.json({
-            code: 404,
-            message: "No task with such credentials found",
-          });
-        }
-        PTOTask.updateOne(
-          {
-            taskId: req.body.taskId,
-            requestorEmail: req.body.requestorEmail,
-          },
-          {
-            ...req.body,
-          }
-        ).then(() => {
-          return res.json({
-            code: 200,
-            message: "Paid Time Off Request updated successfully",
-          });
-        });
-      }
-    );
-  } catch (error) {
-    return res.json(error);
-  }
-});
-router.delete("pto/delete", (req, res, next) => {
-  try {
-    const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
-    const PTOTask = mongoose.connection
-      .useDb(companyDB)
-      .model("PTORequests", ptoSchema);
-
-    PTOTask.findOne({ taskId: req.body.taskId }, (err, task) => {
-      if (!task) {
-        return res.json({ code: 404, message: "Request not found" });
-      }
-      if (task.requestorEmail !== req.body.requestorEmail) {
-        return res.json({
-          code: 401,
-          message: "User does not have deleting permissions",
-        });
-      }
-      PTOTask.delete({
-        taskId: req.body.taskId,
-        requestorEmail: req.body.requestorEmail,
       })
-        .then(() => {
-          return res.json({
-            code: 200,
-            message: "Paid Time Off Request deleted successfully",
-          });
-        })
-        .catch((err) => {
-          return res.json(err);
-        });
-    });
-  } catch (error) {
-    return res.json(error);
-  }
+      .catch((err) => {
+        return res.json(err);
+      });
+  }).catch((err) => {
+    return res.json(err);
+  });
+
+  
+  // try {
+  //   const companyDB = companies.get(req.body.assignerEmail.split("@")[1]);
+  //   const TrainingTask = mongoose.connection
+  //     .useDb(companyDB)
+  //     .model("assignTraining", trainingSchema);
+
+  //   TrainingTask.findOne(
+  //     {
+  //       taskId: req.body.taskId,
+  //       assignerEmail: req.body.assignerEmail,
+  //     },
+  //     (err, task) => {
+  //       if (!task) {
+  //         return res.json({
+  //           code: 404,
+  //           message: "No task with such credentials found",
+  //         });
+  //       }
+  //       TrainingTask.deleteMany({
+  //         taskId: req.body.taskId,
+  //         assignerEmail: req.body.assignerEmail,
+  //       })
+  //         .then(() => {
+  //           return res.json({
+  //             code: 200,
+  //             message: "Tasks deleted successfully",
+  //           });
+  //         })
+  //         .catch((err) => {
+  //           return res.json(err);
+  //         });
+  //     }
+  //   );
+  // } catch (error) {
+  //   return res.json(error);
+  // }
 });
+
+// router.post("/pto/create", (req, res, next) => {
+//   try {
+//     const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
+//     const User = mongoose.connection
+//       .useDb(companyDB)
+//       .model("users", userSchema);
+//     const PTOTask = mongoose.connection
+//       .useDb(companyDB)
+//       .model("PTORequests", ptoSchema);
+
+//     User.findOne({ email: req.body.requestorEmail }, async (err, employee) => {
+//       if (!employee) {
+//         return res.json({ code: 404, message: "Employee not found" });
+//       }
+//       if (employee.positionTitle == "CEO") {
+//         return res.json({
+//           code: 401,
+//           message: "The CEO does not need to request for Paid Time Off",
+//         });
+//       }
+//       User.findOne({ email: req.body.managerEmail }, async (err, manager) => {
+//         if (!manager) {
+//           return res.json({ code: 404, message: "Manager not found" });
+//         }
+//         if (employee.managerId != manager.employeeId) {
+//           return res.json({
+//             code: 401,
+//             message: "Invalid Manager credentials",
+//           });
+//         }
+//         var taskData = {
+//           taskId: Math.abs(
+//             generateHash(employee.email + req.body.startDate + req.body.endDate)
+//           ),
+//           requestorEmail: req.body.requestorEmail,
+//           managerEmail: req.body.managerEmail,
+//           managerId: manager.managerId,
+//           title: req.body.title,
+//           startDate: req.body.startDate,
+//           endDate: req.body.endDate,
+//           reason: req.body.reason,
+//           status: "Pending",
+//         };
+
+//         const ptoRequest = await PTOTask.create(taskData);
+//         await ptoRequest.save();
+//         return res.json({
+//           code: 200,
+//           message: "Successfully added Paid Time Off Request",
+//         });
+//       });
+//     });
+//   } catch (error) {
+//     return res.json(error);
+//   }
+// });
+
+// router.patch("pto/edit", (req, res, next) => {
+//   try {
+//     const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
+//     const PTOTask = mongoose.connection
+//       .useDb(companyDB)
+//       .model("PTORequests", ptoSchema);
+//     PTOTask.findOne(
+//       {
+//         taskId: req.body.taskId,
+//         requestorEmail: req.body.requestorEmail,
+//       },
+//       (err, task) => {
+//         if (!task) {
+//           return res.json({
+//             code: 404,
+//             message: "No task with such credentials found",
+//           });
+//         }
+//         PTOTask.updateOne(
+//           {
+//             taskId: req.body.taskId,
+//             requestorEmail: req.body.requestorEmail,
+//           },
+//           {
+//             ...req.body,
+//           }
+//         ).then(() => {
+//           return res.json({
+//             code: 200,
+//             message: "Paid Time Off Request updated successfully",
+//           });
+//         });
+//       }
+//     );
+//   } catch (error) {
+//     return res.json(error);
+//   }
+// });
+// router.delete("pto/delete", (req, res, next) => {
+//   try {
+//     const companyDB = companies.get(req.body.requestorEmail.split("@")[1]);
+//     const PTOTask = mongoose.connection
+//       .useDb(companyDB)
+//       .model("PTORequests", ptoSchema);
+
+//     PTOTask.findOne({ taskId: req.body.taskId }, (err, task) => {
+//       if (!task) {
+//         return res.json({ code: 404, message: "Request not found" });
+//       }
+//       if (task.requestorEmail !== req.body.requestorEmail) {
+//         return res.json({
+//           code: 401,
+//           message: "User does not have deleting permissions",
+//         });
+//       }
+//       PTOTask.delete({
+//         taskId: req.body.taskId,
+//         requestorEmail: req.body.requestorEmail,
+//       })
+//         .then(() => {
+//           return res.json({
+//             code: 200,
+//             message: "Paid Time Off Request deleted successfully",
+//           });
+//         })
+//         .catch((err) => {
+//           return res.json(err);
+//         });
+//     });
+//   } catch (error) {
+//     return res.json(error);
+//   }
+// });
 
 module.exports = router;

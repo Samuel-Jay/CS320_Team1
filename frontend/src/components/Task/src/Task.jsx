d47deb5ce57868ac9e17077b45e0640df9983766
Cat <cat@1x-nat-vl931-172-30-141-92.wireless.umass.edu>
2022-04-11 17:05:44 -0400

    added checkbox to Task component

--- frontend/src/components/Task/src/Task.jsx
+++ frontend/src/components/Task/src/Task.jsx
@@ -2,13 +2,13 @@ import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {changeStatus} from "../../../actions/Task.js"
 import {Checkbox, Button, Menu, MenuItem}  from "@mui/material";
-
 import CircleCheckedOutline from "@mui/icons-material/CheckCircle";
 import CircleUncheckedOutline from "@mui/icons-material/RadioButtonUnchecked";
 function Task(task){
     const dispatch = useDispatch()
     const[background,setbackground] = useState("#ffff");
     const [anchorEl, setAnchorEl] = useState(null);
+    const [check, setCheck] = useState(false);
     const open = Boolean(anchorEl);
     function handleClose(){
         setAnchorEl(null);
@@ -25,11 +25,31 @@ function Task(task){
         dispatch(changeStatus({task, status}))
     }
 
+    function checked(){}
+
+    function handleChange(){
+        setCheck(prevState => !prevState)
+    }
+
     function open_task(){
     }
     return (
         <div className="EmailRow" style={{ backgroundColor: background }} onClick={open_task}>
+            <div className = "Checkbox">
+                {
+                    check?(
+                        <Checkbox 
+                        onClick={handleChange}/>
+                    ):(                
+                    <Checkbox
+                        checked={checked}
+                        onClick={handleChange}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                    )
+                }
 
+            </div>
             <h3 className=" EmailRow_title">
                 {task.task.taskName}
             </h3>
@@ -43,8 +63,8 @@ function Task(task){
             <div className="time">
                 {task.task.dueDate}
             </div>
-            <div>
-                <Button
+            {/* <div> */}
+                {/* <Button
                     id="basic-button"
                     aria-controls={open ? "basic-menu" : undefined}
                     aria-haspopup="true"
@@ -52,8 +72,8 @@ function Task(task){
                     onClick={handleClick}
                 >
                     Change Status
-                </Button>
-                <Menu
+                </Button> */}
+                {/* <Menu
                     id="basic-menu"
                     anchorEl={anchorEl}
                     open={open}
@@ -65,8 +85,8 @@ function Task(task){
                     <MenuItem onClick={() => {change_status("Archived")}}>Archive</MenuItem>
                     <MenuItem onClick={() => {change_status("Incomplete")}}> Incomplete </MenuItem>
                     <MenuItem onClick={() => {change_status("Completed")}}> Complete </MenuItem>
-                </Menu>
-            </div>
+                </Menu> */}
+            {/* </div> */}
         </div>
     );
 }
